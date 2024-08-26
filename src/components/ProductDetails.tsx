a8225a1fe9526d0e71b3e3435e234232298e18a4
Antonella <98277771+antii16@users.noreply.github.com>
2024-08-26 21:14:47 +0200

    Eliminar Producto

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,9 +1,17 @@
-import { useNavigate } from "react-router-dom"
+import { ActionFunctionArgs, Form, redirect, useNavigate } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
+import { deleteProduct } from "../services/ProductService"
 
 type ProductDetailsProps = {
     product: Product
+}
+
+export async function action({params}: ActionFunctionArgs){
+    if(params.id !== undefined){
+        await deleteProduct(+params.id)
+        return redirect('/')
+    }
 }
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
@@ -18,7 +26,7 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
                 {formatCurrency(product.price)}
             </td>
             <td className="p-3 text-lg text-gray-800">
-                {isAvailable ? 'Disponible': 'No disponible'}
+                {isAvailable ? 'Disponible' : 'No disponible'}
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
@@ -26,6 +34,22 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
                         onClick={() => navigate(`productos/${product.id}/editar`)}
                         className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
                     >Editar</button>
+                    <Form
+                        method="POST"
+                        action={`productos/${product.id}/eliminar`}
+                        className="w-full"
+                        onSubmit={(e) => {
+                            if(!confirm('¿Eliminar?')){
+                                e.preventDefault()
+                            }
+                        }}
+                        >
+                        <input
+                            type="submit"
+                            value='Eliminar'
+                            className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center cursor-pointer"
+                        />
+                    </Form>
                 </div>
             </td>
         </tr>
